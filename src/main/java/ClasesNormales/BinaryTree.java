d1be7deb684ca7809fb92633a11ffe51f00ffe69
jorfxd <jaarroba@espol.edu.ec>
2024-08-17 19:57:57 -0500

    Prueba del metodo del arbol
    se agrego en comentarios para que no perjudique el codigo.

--- src/main/java/ClasesNormales/BinaryTree.java
+++ src/main/java/ClasesNormales/BinaryTree.java
@@ -97,6 +97,119 @@ public class BinaryTree<E> {
             this.addPregunta((E) pregunta); 
         }
     }
+    /*
+    public void crearArbol() throws IOException { 
+        // Cargar el archivo FXML y obtener el controlador
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("src/main/resources/com.mycompany.ed_p1_grupo12_parcial2/Menu"));
+        Parent root = loader.load();
+        MenuController controller = loader.getController();
+        String nombreArchivo = controller.getArchivoPreguntas();
+
+        // Leer preguntas desde archivo
+        String nombre = nombreArchivo; // En caso de no funcionar pon "Preguntas"
+        String direccion = LeerArchivo.texto;
+        LeerArchivo archivo = new LeerArchivo(direccion + nombre + ".txt");
+        ArrayList<String> lineasPreguntas = archivo.leer();
+
+        // Añadir preguntas al árbol
+        for (String pregunta : lineasPreguntas) {
+            this.addPregunta((E) pregunta); 
+        }
+
+        // Leer respuestas desde archivo
+        String nombreRespuesta = controller.getArchivoRespuestas();
+        LeerArchivo archivo2 = new LeerArchivo(direccion + nombreRespuesta + ".txt");
+        ArrayList<String> lineasRespuestas = archivo2.leer();
+
+        // Procesar respuestas
+        for (String respuesta : lineasRespuestas) {
+            String[] lineaDeRespuesta = respuesta.split(" ");
+            Stack<BinaryTree<E>> s = new Stack<>();
+            s.push(this);
+
+            // Iterar sobre las respuestas
+            for (int i = 0; i < lineaDeRespuesta.length; i++) {
+                String contenido = lineaDeRespuesta[i];
+                boolean esHijoIzquierdo = contenido.equals("si");
+                boolean esHijoDerecho = contenido.equals("no");
+
+                if (i > 0) { // Omite el primer elemento, ya que es el nombre del animal
+                    BinaryTree<E> tope = s.peek(); //Para mi es pop
+
+                    // Navegar en el árbol
+                    if (esHijoIzquierdo) {
+                        if (tope.getRoot().getLeft() != null) {
+                            s.push(tope.getRoot().getLeft());
+                        } else {
+                            // Agregar el nodo en el caso de que sea el último paso
+                            if (i == lineaDeRespuesta.length - 1) {
+                                String animal = lineaDeRespuesta[0];
+                                boolean ultimaIzquierda = lineaDeRespuesta[i].equals("si");
+                                this.addNodeToSpecific(tope, (E) animal, ultimaIzquierda);
+                            }
+                        }
+                    } else if (esHijoDerecho) {
+                        if (tope.getRoot().getRight() != null) {
+                            s.push(tope.getRoot().getRight());
+                        } else {
+                            // Agregar el nodo en el caso de que sea el último paso
+                            if (i == lineaDeRespuesta.length - 1) {
+                                String animal = lineaDeRespuesta[0];
+                                boolean ultimaIzquierda = lineaDeRespuesta[i].equals("si");
+                                this.addNodeToSpecific(tope, (E) animal, ultimaIzquierda);
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+    */
+    
+    /*
+    public boolean addPregunta(E contenido) {
+        // Caso base: Si el árbol está vacío, establece la raíz
+        if (this.isEmpty()) {
+            this.root = new NodeBinaryTree<>(contenido);
+            
+            return true;
+        }
+
+        // Usa una pila para recorrer el árbol en profundidad (DFS)
+        Stack<BinaryTree<E>> stack = new Stack<>();
+        stack.push(this);
+
+        while (!stack.isEmpty()) {
+            BinaryTree<E> currentNode = stack.pop();
+
+            // Si el hijo izquierdo es nulo, agrega el nuevo nodo allí
+            if (currentNode.getRoot().getLeft() == null) {
+                BinaryTree<E> newNodeiz= new BinaryTree<>(contenido);
+
+                currentNode.getRoot().setLeft(newNodeiz);
+                currentNode.getRoot().setRight(newNodeiz);
+                
+                return true;
+            } else {
+                stack.push(currentNode.getRoot().getLeft());
+            }
+
+            // Si el hijo derecho es nulo, agrega el nuevo nodo allí
+            if (currentNode.getRoot().getRight() == null) {
+                BinaryTree<E> newNodeiz= new BinaryTree<>(contenido);
+                
+                currentNode.getRoot().setRight(newNodeiz);
+                currentNode.getRoot().setLeft(newNodeiz);
+
+                return true;
+            } else {
+                stack.push(currentNode.getRoot().getRight());
+            }
+        }
+
+        return false; // En teoría, nunca se debería llegar aquí
+    */
     
     //ta bien:)
     public boolean addNodeToSpecific(BinaryTree<E> parentNode, E contenido, boolean isLeft) {

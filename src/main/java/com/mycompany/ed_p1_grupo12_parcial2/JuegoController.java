688ac66656eb0fb9b89e06715d0d3ed866bb6c50
Emily Valarezo <136937944+emilybibi04@users.noreply.github.com>
2024-08-19 00:59:24 -0500

    Completar

--- src/main/java/com/mycompany/ed_p1_grupo12_parcial2/JuegoController.java
+++ src/main/java/com/mycompany/ed_p1_grupo12_parcial2/JuegoController.java
@@ -1,5 +1,7 @@
 package com.mycompany.ed_p1_grupo12_parcial2;
 
+import ClasesNormales.BinaryTree;
+import ClasesNormales.Data;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,7 +11,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,24 +20,63 @@ public class JuegoController implements Initializable {
     @FXML private Button si;
     @FXML private Button no;
     @FXML private Button opciones;
-    @FXML private Label contador;
+    @FXML private Label texto;
+    
+    private BinaryTree<String> arbolPreguntas;
+    private int preguntasRestantes;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        arbolPreguntas = Data.getArbolPreguntas(); 
+        preguntasRestantes = Data.getCantidadPreguntas();
+
+        if (arbolPreguntas != null && arbolPreguntas.getRoot() != null) {
+            texto.setText(arbolPreguntas.getRoot().getContent());
+        }
+//        //Ver numero de pregunta 
+//        contador.setText("Preguntas restantes: " + preguntasRestantes);
     }
     
     private void OpcionSi() {
-        
+        avanzarJuego(true);
     }
     
     private void OpcionNo() {
-        
+        avanzarJuego(false);
     }
     
     private boolean juegoTerminado(){
-        boolean valor = true;
-        return valor;
+        return true;
+    }
+    
+    private void avanzarJuego(boolean si) {
+        if (arbolPreguntas != null) {
+            if (si) {
+                arbolPreguntas = arbolPreguntas.getRoot().getLeft();
+            } else {
+                arbolPreguntas = arbolPreguntas.getRoot().getRight();
+            }
+
+            preguntasRestantes--;
+
+
+            if (preguntasRestantes > 0 && arbolPreguntas != null && !arbolPreguntas.isLeaf()) {
+                texto.setText(arbolPreguntas.getRoot().getContent());
+//                contador.setText("Resta: " + preguntasRestantes);
+            } else {
+                terminarJuego();
+            }
+        }
+    }
+    
+    private void terminarJuego() {
+        if (arbolPreguntas != null && arbolPreguntas.isLeaf()) {
+            texto.setText("Respuesta final: " + arbolPreguntas.getRoot().getContent());
+        } else  { //Aqui se implementar crear lista de animales que
+            texto.setText(" No preguntas");
+        }
+        si.setDisable(true);
+        no.setDisable(true);
     }
     
     @FXML
